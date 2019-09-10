dec70ebfda6ff00721614cb849829387201b26b6
Charlie Camilleri <cvdcamilleri@gmail.com>
2019-09-10 19:45:56 +0100

    oops

--- fireflyd.py
+++ fireflyd.py
@@ -7,7 +7,8 @@ from fireflyd_lib import *
 print("[ fireflyd copyright Charlie Camilleri 2019 ]")
 
 print("[ logging in ]")
-cookies = login("C_Camilleri@wincoll.ac.uk","13@Fghan")
+cookies = login(u,p)
 print("[ logged in ]")
+
 tasks = get_tasks(cookies=cookies)
 print("[ downloaded tasks ]")
