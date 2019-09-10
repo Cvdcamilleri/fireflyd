c6b7ec18e3ea2063a2ed087ae1cfdbefed95ce73
Charlie Camilleri <cvdcamilleri@gmail.com>
2019-09-10 19:47:27 +0100

    added private upcache

--- fireflyd.py
+++ fireflyd.py
@@ -3,11 +3,12 @@
 # fireflyd, Copyright Charlie Camilleri 2018
 
 from fireflyd_lib import *
+import up_cache
 
 print("[ fireflyd copyright Charlie Camilleri 2019 ]")
 
 print("[ logging in ]")
-cookies = login(u,p)
+cookies = login(u(),p())
 print("[ logged in ]")
 
 tasks = get_tasks(cookies=cookies)
